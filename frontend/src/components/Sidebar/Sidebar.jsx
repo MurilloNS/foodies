bba12bf74c9c58f4e8a6d0b941c364662ac7822b
Murillo <murillo.murillo2001@gmail.com>
2025-12-24 21:51:50 -0300

    Adding React Router, images, components and pages

---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = ({ sidebarVisible }) => {
+  return (
+    <div
+      className={`border-end bg-white ${sidebarVisible ? "" : "d-none"}`}
+      id="sidebar-wrapper"
+    >
+      <div className="sidebar-heading border-bottom bg-light">
+        Start Bootstrap
+      </div>
+      <div className="list-group list-group-flush">
+        <Link
+          className="list-group-item list-group-item-action list-group-item-light p-3"
+          to="/add"
+        >
+          Add Food
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action list-group-item-light p-3"
+          to="/list"
+        >
+          List Foods
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action list-group-item-light p-3"
+          to="/orders"
+        >
+          Orders
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
