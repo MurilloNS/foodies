de5206bf69bea33f897ad7b1e81879fa4c5564dc
Murillo <murillo.murillo2001@gmail.com>
2026-02-16 15:16:39 -0300

    Creating application-test

--- backend/foodiesapi/src/test/java/com/ollirum/foodiesapi/FoodiesapiApplicationTests.java
+++ backend/foodiesapi/src/test/java/com/ollirum/foodiesapi/FoodiesapiApplicationTests.java
@@ -2,8 +2,10 @@ package com.ollirum.foodiesapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FoodiesapiApplicationTests {
 
 	@Test
